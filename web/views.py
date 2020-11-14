cca9d1f8b7b510fdc96bfcd8d36507406a53fb0e
adenurhidayat <adenurhidayat101294@gmail.com>
2020-11-14 23:26:32 +0700

    detail to olx

--- web/views.py
+++ web/views.py
@@ -28,16 +28,16 @@ def index(request):
             Products.append(make_product(namaBarang.text,harga.text,link,deskripsi,lokasi.text,img,waktu.text))
            
     # for b in Products:
-    #     print(b.nama_barang)
+    #     print(b.link_barang)
     context={
         'Products' : Products
     } 
     return render(request,'index.html',context)
 
 def search_product(request):
-    print(request.POST.get('product'))
     search_product = request.POST.get('product')
     headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:79.0) Gecko/20100101 Firefox/79.0'}
+    base_url = 'https://www.olx.co.id'
     url = 'https://www.olx.co.id/items/q-' + search_product
     page = requests.get(url, headers=headers)
     soup = BeautifulSoup(page.text, 'html.parser')
@@ -49,7 +49,7 @@ def search_product(request):
             namaBarang = a.find("span",{"data-aut-id": "itemTitle"})
             lokasi = a.find("span",{"data-aut-id": "item-location"})
             link = a.find("a",href=True)
-            link = url+link['href']
+            link = base_url+link['href']
             img = a.find("img")
             img = img['src']
             waktu = a.find("span",{"class": "zLvFQ"})
@@ -57,6 +57,8 @@ def search_product(request):
             deskripsi = a.find("span",{"data-aut-id": "itemDetails"})
             lokasi = a.find("span",{"data-aut-id": "item-location"})
             Products.append(make_product(namaBarang.text,harga.text,link,deskripsi,lokasi.text,img,waktu.text))
+    # for b in Products:
+    #     print(b.link_barang)
     context={
         'Products' : Products
     } 
