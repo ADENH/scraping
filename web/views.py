5010e6ef4a220db244930a42bc1e727bd2222b96
adenurhidayat <adenurhidayat101294@gmail.com>
2020-11-15 14:23:00 +0700

    reusable method

--- web/views.py
+++ web/views.py
@@ -13,19 +13,7 @@ def index(request):
     Products = []
     if page.status_code==200:
         div = soup.findAll("li",{"data-aut-id": "itemBox"})
-        for a in div:
-            harga = a.find("span",{"data-aut-id": "itemPrice"})
-            namaBarang = a.find("span",{"data-aut-id": "itemTitle"})
-            lokasi = a.find("span",{"data-aut-id": "item-location"})
-            link = a.find("a",href=True)
-            link = url+link['href']
-            img = a.find("img")
-            img = img['src']
-            waktu = a.find("span",{"class": "zLvFQ"})
-            waktu = waktu.find('span')
-            deskripsi = a.find("span",{"data-aut-id": "itemDetails"})
-            lokasi = a.find("span",{"data-aut-id": "item-location"})
-            Products.append(make_product(namaBarang.text,harga.text,link,deskripsi,lokasi.text,img,waktu.text))
+        Products = set_product(div,url)
            
     # for b in Products:
     #     print(b.link_barang)
@@ -46,19 +34,7 @@ def search_product(request):
     Products = []
     if page.status_code==200:
         div = soup.findAll("li",{"data-aut-id": "itemBox"})
-        for a in div:
-            harga = a.find("span",{"data-aut-id": "itemPrice"})
-            namaBarang = a.find("span",{"data-aut-id": "itemTitle"})
-            lokasi = a.find("span",{"data-aut-id": "item-location"})
-            link = a.find("a",href=True)
-            link = base_url+link['href']
-            img = a.find("img")
-            img = img['src']
-            waktu = a.find("span",{"class": "zLvFQ"})
-            waktu = waktu.find('span')
-            deskripsi = a.find("span",{"data-aut-id": "itemDetails"})
-            lokasi = a.find("span",{"data-aut-id": "item-location"})
-            Products.append(make_product(namaBarang.text,harga.text,link,deskripsi,lokasi.text,img,waktu.text))
+        Products = set_product(div,base_url)
     # for b in Products:
     #     print(b.link_barang)
     title = 'Hasil Pencarian '+search_product
@@ -78,19 +54,7 @@ def mobil_bekas(request):
     Products = []
     if page.status_code==200:
         div = soup.findAll("li",{"data-aut-id": "itemBox"})
-        for a in div:
-            harga = a.find("span",{"data-aut-id": "itemPrice"})
-            namaBarang = a.find("span",{"data-aut-id": "itemTitle"})
-            lokasi = a.find("span",{"data-aut-id": "item-location"})
-            link = a.find("a",href=True)
-            link = base_url+link['href']
-            img = a.find("img")
-            img = img['src']
-            waktu = a.find("span",{"class": "zLvFQ"})
-            waktu = waktu.find('span')
-            deskripsi = a.find("span",{"data-aut-id": "itemDetails"})
-            lokasi = a.find("span",{"data-aut-id": "item-location"})
-            Products.append(make_product(namaBarang.text,harga.text,link,deskripsi,lokasi.text,img,waktu.text))
+        Products = set_product(div,base_url)
     title = 'Mobil Bekas'
     context={
         'Products' : Products,
@@ -103,27 +67,33 @@ def motor_bekas(request):
     headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:79.0) Gecko/20100101 Firefox/79.0'}
     base_url = 'https://www.olx.co.id'
     url = 'https://www.olx.co.id/motor-bekas_c200'
+    # url_api ='https://www.olx.co.id/api/relevance/search?facet_limit=100&location=1000001&location_facet_limit=20&page=1&query='+search_product+'&spellcheck=true&user=0'
     page = requests.get(url, headers=headers)
     soup = BeautifulSoup(page.text, 'html.parser')
     Products = []
     if page.status_code==200:
         div = soup.findAll("li",{"data-aut-id": "itemBox"})
-        for a in div:
-            harga = a.find("span",{"data-aut-id": "itemPrice"})
-            namaBarang = a.find("span",{"data-aut-id": "itemTitle"})
-            lokasi = a.find("span",{"data-aut-id": "item-location"})
-            link = a.find("a",href=True)
-            link = base_url+link['href']
-            img = a.find("img")
-            img = img['src']
-            waktu = a.find("span",{"class": "zLvFQ"})
-            waktu = waktu.find('span')
-            deskripsi = a.find("span",{"data-aut-id": "itemDetails"})
-            lokasi = a.find("span",{"data-aut-id": "item-location"})
-            Products.append(make_product(namaBarang.text,harga.text,link,deskripsi,lokasi.text,img,waktu.text))
+        Products = set_product(div,base_url)
     title = 'Motor Bekas'
     context={
         'Products' : Products,
         'Title' : title
     } 
     return render(request,'index.html',context)
+
+def set_product(div,base_url):
+    Products = []
+    for a in div:
+        harga = a.find("span",{"data-aut-id": "itemPrice"})
+        namaBarang = a.find("span",{"data-aut-id": "itemTitle"})
+        lokasi = a.find("span",{"data-aut-id": "item-location"})
+        link = a.find("a",href=True)
+        link = base_url+link['href']
+        img = a.find("img")
+        img = img['src']
+        waktu = a.find("span",{"class": "zLvFQ"})
+        waktu = waktu.find('span')
+        deskripsi = a.find("span",{"data-aut-id": "itemDetails"})
+        lokasi = a.find("span",{"data-aut-id": "item-location"})
+        Products.append(make_product(namaBarang.text,harga.text,link,deskripsi,lokasi.text,img,waktu.text))
+    return Products
